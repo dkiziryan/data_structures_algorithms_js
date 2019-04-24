ee0c2b8106e1a9c6d787f1be6db33addb753058a
dkiziryan <dkiziryan@gmail.com>
2019-04-24 10:42:34 -0700

    stack with queues

---
+++ src/data_structures/stackUsingQueues.js
@@ -0,0 +1,55 @@
+const Queue = require('./queue');
+
+class StackWithQueues {
+
+    constructor() {
+        this.q1 = new Queue();
+        this.q2 = new Queue();
+        this.top;
+    }
+
+    push(item) {
+        this.q1.enqueue(item)
+        this.top = item;
+    }
+
+    pop() {
+
+        if (this.q1.isEmpty()) {
+            console.log('cannot pop an empty stack')
+            return;
+        }
+
+        while (this.q1.size() > 1) {
+            this.top = this.q1.dequeue()
+            this.q2.enqueue(this.top);
+        }
+
+        let returnValue = this.q1.dequeue();
+
+        let temp = this.q1;
+        this.q1 = this.q2;
+        this.q2 = temp;
+
+        return returnValue;
+    }
+
+    topOfStack() {
+        return this.top;
+    }
+
+    empty() {
+        return this.q1.isEmpty();
+    }
+}
+
+let sq = new StackWithQueues();
+
+sq.push(1);
+sq.push(2);
+sq.push(3);
+console.log('should be 3, ===', sq.topOfStack())
+sq.pop();
+sq.pop();
+sq.pop();
+console.log(sq.empty())
